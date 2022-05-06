f1e17f68022cf845b3e33264e9d67b02c8d40a09
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-05-06 04:04:43 +0200

    Feat: DateTime in Taskbar

---
+++ src/components/DayTime.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+const getTime = (time: number) => {
+  const options: Intl.DateTimeFormatOptions = {
+    hour: 'numeric',
+    minute: '2-digit',
+  };
+  return Intl.DateTimeFormat(undefined, options).format(time);
+};
+
+const getDate = (time: number) => {
+  const options: Intl.DateTimeFormatOptions = {
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric',
+  };
+  return Intl.DateTimeFormat(undefined, options).format(time);
+};
+
+const DayTime: React.FC = () => {
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    const timeout = window.setTimeout(() => { setTime(Date.now()); }, 1000);
+    return () => clearTimeout(timeout);
+  }, [time]);
+
+  return (
+    <div
+      className="ml-auto text-white flex flex-col justify-between text-center text-xs px-2 pt-0.5 pb-1"
+    >
+      <p>{ getTime(time) }</p>
+      <p>{ getDate(time) }</p>
+    </div>
+  );
+};
+
+export default DayTime;
--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -1,5 +1,9 @@
+import DayTime from './DayTime';
+
 const Taskbar: React.FC = () => (
-  <div className="shrink-0 bg-rose-900 h-10">&nbsp;</div>
+  <div className="shrink-0 bg-rose-900 h-10 flex">
+    <DayTime />
+  </div>
 );
 
 export default Taskbar;
