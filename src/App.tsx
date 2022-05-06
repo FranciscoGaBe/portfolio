45f4b88cc41a5defb59886de6bbdeb4e541e2e91
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-05-06 03:36:34 +0200

    Feat: Taskbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Desktop from './components/Desktop';
+import Taskbar from './components/Taskbar';
 import WelcomeScreen from './components/WelcomeScreen';
 
 const App: React.FC = () => (
   <div className="h-full flex flex-col">
     <Desktop />
+    <Taskbar />
     <WelcomeScreen />
   </div>
 );
---
+++ src/components/Taskbar.tsx
@@ -0,0 +1,5 @@
+const Taskbar: React.FC = () => (
+  <div className="shrink-0 bg-rose-900 h-10">&nbsp;</div>
+);
+
+export default Taskbar;
